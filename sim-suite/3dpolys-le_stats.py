a770750d7a47235d4cd324cdb380b9fb5d549b35
Todor <todor.gitchev@gmail.com>
2021-04-09 15:31:57 +0000

    Update 3dpolys-le_stats.py: comment the line

--- sim-suite/3dpolys-le_stats.py
+++ sim-suite/3dpolys-le_stats.py
@@ -144,7 +144,7 @@ else:
 
 chip_out_file = os.path.join(args.analyse, ha.CHIP_OUT)
 chip_corr = 0, 0, 0  # ha.get_chip_correlation(chip_out_file, args.exp_chip, args.boundary, args.bin_size,
-                                    correlation=chip_correlation, plot=True, replace=args.replace)
+                    #                correlation=chip_correlation, plot=True, replace=args.replace)
 
 # import matplotlib.pyplot as plt
 # plt.plot(exp_score, sim_score, '.')  # juts to visualize with what the correlation coefficient has to deal with
