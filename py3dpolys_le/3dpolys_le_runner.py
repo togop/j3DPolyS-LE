51b5c942f924d7b74c3fd602536b828922515606
Todor Gitchev <todor.gitchev@gmail.com>
2021-04-30 09:45:21 +0200

    rename cmd_in to cmd_run; rename data files; update version

--- py3dpolys_le/3dpolys_le_runner.py
+++ py3dpolys_le/3dpolys_le_runner.py
@@ -140,7 +140,7 @@ p.add_argument("--bin_size", default=1, help="Chip-seq bin size used for plottin
 p.add_argument("--correlation", default='spearman', choices=['spearmanr', 'pearsonr'],
                help="Correlation method to use to compare Chip-seq profiles.")
 p.add_argument("-o", "--output_folder", default=".", help="Simulation output folder.")
-p.add_argument("--cmd_in_file", default="", help="File where to save all commands.")
+p.add_argument("--cmd_run_file", default="", help="File where to save all commands.")
 p.add_argument("-a", "--analysis_folder", default="", help="Analysis output folder: Hi-C, Chip-Seq in silico.")
 #p.add_argument("-jr", "--job_runner", default='slurm', choices=['slurm', 'shell', 'dummy'],
 #               help="Environment mode to run jobs.  Default: slurm")
@@ -606,7 +606,7 @@ def main():
                        f'with which HiCs will be compared!')
         ha.CHR_SYNONYMS = args.cmp_chrs
 
-    job_runner: JobRunner = CfgJobRunner(input_cfg=args.input_cfg, cmd_in_file=args.cmd_in_file)
+    job_runner: JobRunner = CfgJobRunner(input_cfg=args.input_cfg, cmd_run_file=args.cmd_run_file)
 
     dcc_args = DccExtrusionArgs(boundary=args.boundary, lef_binding_sites=args.lef_binding_sites,
                                 input_cfg=args.input_cfg, tads_boundary=args.tads_boundary,
--- py3dpolys_le/job_runner.py
+++ py3dpolys_le/job_runner.py
@@ -19,7 +19,7 @@ class JobRunner(ABC):
         full_cmd = f"{start_cmd} {cmd}"
 
         jobid = ''
-        if self._cmd_in_shell(profile):
+        if self._cmd_run_shell(profile):
             status, jobout = subprocess.getstatusoutput(full_cmd)
             logger.info(f" call: {full_cmd}\n\t {jobout}")
             if status == 0:
@@ -27,15 +27,15 @@ class JobRunner(ABC):
                 logger.info(f"JobID is: {jobid}")
             else:
                 logger.error(f"Error submitting Job: {full_cmd}")
-        elif self._cmd_in_stdout(profile):
+        elif self._cmd_run_stdout(profile):
             print(full_cmd)
         else:
-            cmd_in_file = self._cmd_in_file(profile)
-            if cmd_in_file:
-                if not os.path.isfile(cmd_in_file):
-                    with open(cmd_in_file, 'w') as f:
+            cmd_run_file = self._cmd_run_file(profile)
+            if cmd_run_file:
+                if not os.path.isfile(cmd_run_file):
+                    with open(cmd_run_file, 'w') as f:
                         print('', file=f)
-                with open(cmd_in_file, 'a') as f:
+                with open(cmd_run_file, 'a') as f:
                     print(full_cmd, file=f)
         return jobid
 
@@ -48,26 +48,26 @@ class JobRunner(ABC):
         pass
 
     @abstractmethod
-    def _cmd_in_shell(self, profile) -> bool:
+    def _cmd_run_shell(self, profile) -> bool:
         pass
 
     @abstractmethod
-    def _cmd_in_stdout(self, profile) -> bool:
+    def _cmd_run_stdout(self, profile) -> bool:
         pass
 
     @abstractmethod
-    def _cmd_in_file(self, profile) -> str:
+    def _cmd_run_file(self, profile) -> str:
         pass
 
 
 class CfgJobRunner(JobRunner):
     input_cfg: str
-    cmd_in_file: str
+    cmd_run_file: str
     _config = configparser.ConfigParser()
 
-    def __init__(self, input_cfg, cmd_in_file):
+    def __init__(self, input_cfg, cmd_run_file):
         self.input_cfg = input_cfg
-        self.cmd_in_file = cmd_in_file
+        self.cmd_run_file = cmd_run_file
         self._config.read(self.input_cfg)
 
     def _get_start_cmd(self, dep_jobid, profile) -> str:
@@ -91,17 +91,17 @@ class CfgJobRunner(JobRunner):
             value = self._config.get('job_runner', name)
         return value
 
-    def _cmd_in_shell(self, profile) -> bool:
-        cmd_in = self._get_property(profile, 'cmd_in')
-        return cmd_in == 'shell'
+    def _cmd_run_shell(self, profile) -> bool:
+        cmd_run = self._get_property(profile, 'cmd_run')
+        return cmd_run == 'shell'
 
-    def _cmd_in_stdout(self, profile) -> bool:
-        cmd_in = self._get_property(profile, 'cmd_in')
-        return cmd_in == 'stdout'
+    def _cmd_run_stdout(self, profile) -> bool:
+        cmd_run = self._get_property(profile, 'cmd_run')
+        return cmd_run == 'stdout'
 
-    def _cmd_in_file(self, profile) -> str:
-        cmd_in = self._get_property(profile, 'cmd_in')
-        if cmd_in.startswith('file:'):
-            cmd_in_file = cmd_in.split(':')[1]
-            return cmd_in_file.replace('{cmd_in_file}', self.cmd_in_file)
+    def _cmd_run_file(self, profile) -> str:
+        cmd_run = self._get_property(profile, 'cmd_run')
+        if cmd_run.startswith('file:'):
+            cmd_run_file = cmd_run.split(':')[1]
+            return cmd_run_file.replace('{cmd_run_file}', self.cmd_run_file)
         return ''
