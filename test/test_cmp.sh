48bc38fcc66a44460fcffaa5a1d1004734cf18ff
Todor Gitchev <todor.gitchev@gmail.com>
2021-05-13 14:16:57 +0200

    try another file compare

--- test/test_cmp.sh
+++ test/test_cmp.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if cmp -s "$1" "$2"; then
+diff -a "$1" "$2";>/dev/null;CMP=$?
+# if cmp -s "$1" "$2"; then
+if [ ${CMP} -eq 0 ]; then
     echo "TEST OK: File $1 is the same as expected $2"
 else
     echo "TEST FAILED: File $1 is not the same as expected  $2"
