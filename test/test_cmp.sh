a4a0639d122d500352a8414c1c1c7da079cb6e3c
Todor Gitchev <todor.gitchev@gmail.com>
2021-05-13 14:24:37 +0200

    make run script executable

--- test/test_cmp.sh
+++ test/test_cmp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-diff -a "$1" "$2";>/dev/null;CMP=$?
+diff -w "$1" "$2";>/dev/null;CMP=$?
 # if cmp -s "$1" "$2"; then
 if [ ${CMP} -eq 0 ]; then
     echo "TEST OK: File $1 is the same as expected $2"
