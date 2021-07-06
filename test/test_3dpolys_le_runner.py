d3cd6610a83e01ed40fc459a1429680861d6d5f4
Todor Gitchev <todor.gitchev@gmail.com>
2021-07-06 10:48:32 +0200

    update test_shell_container result

--- test/test_3dpolys_le_runner.py
+++ test/test_3dpolys_le_runner.py
@@ -8,8 +8,6 @@ import filecmp
 import pytest
 from py3dpolys_le import plot_hic
 
-from abc import ABC, abstractmethod
-
 # Initialization
 logger = logging.getLogger(__name__)
 
@@ -23,6 +21,24 @@ def is_file_created(file_path, timeout=300):  # max 15min
     return os.path.isfile(file_path)
 
 
+def cmp_file_txt(expected_shel_script, shel_script):
+    status, stout = subprocess.getstatusoutput(f'./test/test_cmp.sh {expected_shel_script} {shel_script}')
+    if status != 0:  # stout.strip().index('TEST OK:') < 0:  # not filecmp.cmp(shel_script, expected_shel_script):
+        f = open(expected_shel_script, 'r')
+        content = f.read()
+        print('Expected:\n')
+        print(content)
+        f.close()
+
+        f = open(shel_script, 'r')
+        content = f.read()
+        print('Got:\n')
+        print(content)
+        f.close()
+        return False
+    return True
+
+
 def test_no_tads_shell():
     shel_script = "./run_test_no_tads_shell_input.sh"
     if os.path.exists(shel_script):
@@ -32,6 +48,7 @@ def test_no_tads_shell():
     print(f"call: {cmd}")
     subprocess.run(cmd, shell=True, check=True)
     assert is_file_created(shel_script), f"Shell script {shel_script} file is not created"
+    # assert cmp_file_txt(expected_shel_script, shel_script), f"Shell script {shel_script} is not as expected: "
     subprocess.run(f"chmod +x {shel_script}", shell=True, check=True)
     subprocess.run(shel_script, shell=True, check=True)
 
@@ -46,20 +63,7 @@ def test_shell_container():
     subprocess.run(cmd, shell=True, check=True)
     assert is_file_created(shel_script), f"Shell script {shel_script} file is not created"
     expected_shel_script = "./test/expected/run_test_shell_container_input.sh"
-    status, stout = subprocess.getstatusoutput(f'./test/test_cmp.sh {expected_shel_script} {shel_script}')
-    if status != 0:  # stout.strip().index('TEST OK:') < 0:  # not filecmp.cmp(shel_script, expected_shel_script):
-        f = open(expected_shel_script, 'r')
-        content = f.read()
-        print('Expected:\n')
-        print(content)
-        f.close()
-
-        f = open(shel_script, 'r')
-        content = f.read()
-        print('Got:\n')
-        print(content)
-        f.close()
-        assert False, f"Shell script {shel_script} is not as expected: "
+    assert cmp_file_txt(expected_shel_script, shel_script), f"Shell script {shel_script} is not as expected: "
 
 
 if __name__ == '__main__':
